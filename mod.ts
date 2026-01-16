7206d09e6ff3ac7ccbb203dfe2fc5eedc27a3d36
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2026-01-17 01:54:16 +0900

    chore: deno fmt

--- mod.ts
+++ mod.ts
@@ -1,2 +1,2 @@
 export { ensure, lazy } from "./src/api.ts";
-export type { Context, Dev, HookSpec, Hooks, Lazy, Plugin } from "./src/types.ts";
+export type { Context, Dev, Hooks, HookSpec, Lazy, Plugin } from "./src/types.ts";
